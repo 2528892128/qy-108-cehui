312a633a5a6aa5a1b8ec509c3bc21aeaff552418
yu <1666375465@qq.com>
2020-05-27 14:03:41 +0800

    Create DictController.java

---
+++ qy-108-provider-management/qy-108-system-provider-6081/src/main/java/com/aaa/xj/controller/DictController.java
@@ -0,0 +1,83 @@
+package com.aaa.xj.controller;
+
+import com.aaa.xj.model.Dict;
+import com.aaa.xj.service.DictService;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @program: qy-108-cehui
+ * @description:
+ * @author: ygy
+ * @create: 2020-05-26-2020/5/26 16:42
+ */
+@RestController
+public class DictController {
+
+    @Autowired
+    private DictService dictService;
+
+    /**
+     * @Description: 查询字典信息
+     * @Param: []
+     * @return: java.util.List<com.aaa.xj.model.Dict>
+     * @Author: ygy
+     * @Date: 2020/5/26 16:43
+     */
+    @PostMapping("/queryDict")
+    public List<Dict> selectDict(){
+        try{
+            List<Dict> dicts = dictService.selectDict();
+            return dicts;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * @Description: 字典信息分页查询
+     * @Param: [dict, pageNo, pageSize]
+     * @return: com.github.pagehelper.PageInfo
+     * @Author: ygy
+     * @Date: 2020/5/26 19:56
+     */
+    @PostMapping("/queryDictAllPage")
+    public PageInfo<Dict> selectAllDictByPage(@RequestBody Dict dict, @RequestParam("pageNo") Integer pageNo, @RequestParam("pageSize") Integer pageSize){
+        try{
+            PageInfo pageInfo = dictService.selectAllDictByPage(dict, pageNo, pageSize);
+            return pageInfo;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+
+
+    /**
+     * @Description: 字典信息条件查询
+     * @Param: [dict]
+     * @return: java.util.List<com.aaa.xj.model.Dict>
+     * @Author: ygy
+     * @Date: 2020/5/26 18:04
+     */
+    @PostMapping("/queryDictList")
+    public List<Dict> selectDictList(@RequestBody Dict dict){
+        try {
+            List<Dict> dicts = dictService.queryList(dict);
+            return dicts;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
