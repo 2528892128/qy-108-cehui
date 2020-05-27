bf43bef9cb19dfbc122df63d7f0d24503ca93570
yu <1666375465@qq.com>
2020-05-27 14:02:59 +0800

    Create Dict.java

---
+++ qy-108-management/qy-108-model/src/main/java/com/aaa/xj/model/Dict.java
@@ -0,0 +1,49 @@
+package com.aaa.xj.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+@Table(name = "t_dict")
+public class Dict implements Serializable {
+    /**
+     * 字典ID
+     */
+    @Id
+    @Column(name = "DICT_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long dictId;
+
+    /**
+     * 键
+     */
+    @Column(name = "KEYY")
+    private Long keyy;
+
+    /**
+     * 值
+     */
+    @Column(name = "VALUEE")
+    private String valuee;
+
+    /**
+     * 字段名称
+     */
+    @Column(name = "FIELD_NAME")
+    private String fieldName;
+
+    /**
+     * 表名
+     */
+    @Column(name = "TABLE_NAME")
+    private String tableName;
+
+}
