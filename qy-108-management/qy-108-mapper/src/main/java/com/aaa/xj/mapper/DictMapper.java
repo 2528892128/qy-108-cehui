6b2279d94bc50448dcbd4045acd8ba73e07554eb
yu <1666375465@qq.com>
2020-05-29 11:15:44 +0800

    字典代码

--- qy-108-management/qy-108-mapper/src/main/java/com/aaa/xj/mapper/DictMapper.java
+++ qy-108-management/qy-108-mapper/src/main/java/com/aaa/xj/mapper/DictMapper.java
@@ -4,4 +4,9 @@ import com.aaa.xj.model.Dict;
 import tk.mybatis.mapper.common.Mapper;
 
 public interface DictMapper extends Mapper<Dict> {
+
+    Dict selectUpdateDict(Long id);
+
+    Integer updateDict(Dict dict);
+
 }
