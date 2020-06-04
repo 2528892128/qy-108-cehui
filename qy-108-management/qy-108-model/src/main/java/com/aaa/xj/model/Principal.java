589cac66eef305e09711cd3ab7c546c8dba9e7d6
yu <1666375465@qq.com>
2020-06-04 09:01:02 +0800

    Update Principal.java

--- qy-108-management/qy-108-model/src/main/java/com/aaa/xj/model/Principal.java
+++ qy-108-management/qy-108-model/src/main/java/com/aaa/xj/model/Principal.java
@@ -8,6 +8,7 @@ import lombok.experimental.Accessors;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -98,5 +99,12 @@ public class Principal implements Serializable {
      */
     @Column(name = "modify_time")
     private String modifyTime;
+
+
+    /**
+     * 一对多，对应多个资源
+     */
+    private List<Resource> resources;
+
 
 }
