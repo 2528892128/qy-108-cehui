2d979fe7a09438b3a9f5ca083557bea116a96b9a
yu <1666375465@qq.com>
2020-06-04 09:01:26 +0800

    Update ManProjectService.java

--- qy-108-management/qy-108-service/src/main/java/com/aaa/xj/service/ManProjectService.java
+++ qy-108-management/qy-108-service/src/main/java/com/aaa/xj/service/ManProjectService.java
@@ -1,10 +1,12 @@
 package com.aaa.xj.service;
 
+import com.aaa.xj.base.BaseModel;
 import com.aaa.xj.base.BaseService;
 import com.aaa.xj.base.ResultData;
 import com.aaa.xj.mapper.ManProjectMapper;
 import com.aaa.xj.model.ManProject;
 import com.aaa.xj.model.User;
+import com.aaa.xj.utils.FileNameUtils;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +14,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 import static com.aaa.xj.status.CrudStatus.*;
 
@@ -125,6 +126,42 @@ public class ManProjectService extends BaseService<ManProject> {
             e.printStackTrace();
         }
         return null;
+    }
+
+    /**
+     * @Description: 新增测绘项目信息
+     * @Param: [manProject]
+     * @return: java.lang.Integer
+     * @Author: ygy
+     * @Date: 2020/6/3 23:31
+     */
+    public Integer addManProject(ManProject manProject){
+
+        if (null != manProject && !"".equals(manProject)){
+            //获取时间
+        Date date = new Date();
+        //设置时间格式作为创建时间
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
+        String format = simpleDateFormat.format(date);
+            // 获取系统当前时间的毫秒数
+        Long timeMillis = System.currentTimeMillis();
+            // 创建Random对象
+        Random random = new Random();
+        // 做一个随机数，随机区间是0-9999之间随机
+        Integer randomNum = random.nextInt(9999);
+        //把系统当前时间和randomNum相加作为id
+        Long id = randomNum + timeMillis;
+            manProject.setId(id)
+                    .setCreateTime(format);
+
+            int insert = manProjectMapper.insert(manProject);
+
+            if (insert > 0){
+                return insert;
+            }
+            return 0;
+        }
+        return 0;
     }
 
 
